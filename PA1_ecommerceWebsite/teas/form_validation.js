66ae1a7278b8acfd4423cedf0f6fcbcaf6d26462
aliyah-qt <aliyahclayton.f@gmail.com>
2021-04-15 14:52:59 -0700

    Completeed validation for street address and optional apt fill-in. Form validation is now complete.

--- PA1_ecommerceWebsite/teas/form_validation.js
+++ PA1_ecommerceWebsite/teas/form_validation.js
@@ -9,12 +9,27 @@ function validateForm(){
   }
 
   // For street
-  var pattern = /^\d+\s[A-z]+\s[A-z]+/;
-  var street = document.getElementsByName("addr").value;
+  var street_pattern = /^[0-9][0-9][0-9](?:[0-9])?(\s[A-za-z]+){2,}$/;
+  /* ^[0-9] three times means string must begin with at least 3 digits
+     (?:[0-9])? means it's optional to have a fourth digit at beginning of string
+     (\s[A-za-z]+) cannot contain digits, only alpha characters
+     {2,} is at least 2 repetitions
+  */
 
-  if(pattern.test(street) == false)
+  var street = document.getElementById("addr").value;
+
+  if(street_pattern.test(street) == false)
   {
     alert("Please input a valid street address.");
+    submitOK = "false";
+  }
+
+  //For apt/suite/bldg
+  var apt_pattern = /(Apt|Suite|Ste|Bldg)\s\d+$/;
+  var apt = document.getElementById("addr2").value;
+
+  if((apt != "") && (apt_pattern.test(apt) == false)){
+    alert("Please input a valid apt/suite/bldg");
     submitOK = "false";
   }
 
